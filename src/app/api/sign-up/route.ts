db5fc5cbfbd4650949d098ce6ef5ce9c6b307a9b
Ofonna Nweze <nwezeofonna@gmail.com>
2025-09-30 00:42:04 -0500

    Add sign-up route with user validation and session creation

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,83 @@
+import { createSession, createUser } from "@/src/features/auth/libs/auth";
+import { Prisma } from "@/src/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { success, z } from "zod";
+
+const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters long" })
+    .max(100),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(100),
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const validation = signUpSchema.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(
+        {
+          error: "Invalid input",
+          issues: validation.error.issues,
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username, password } = validation.data;
+
+    const user = await createUser({ username, password });
+    const token = await createSession({
+      userId: user.id,
+    });
+
+    const userProfileUrl = new URL(
+      `/api/users/${user.id}`,
+      request.nextUrl.origin
+    );
+
+    const response = NextResponse.json(
+      {
+        success: true,
+        message: "User created successfully",
+      },
+      {
+        status: 201,
+        headers: {
+          Location: userProfileUrl.toString(),
+        },
+      }
+    );
+
+    response.cookies.set("sessionToken", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24, // 1 day in seconds
+      path: "/",
+    });
+    // Check if user already exists
+  } catch (err) {
+    console.error("Error parsing request body:", err);
+
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === "P2002"
+    ) {
+      return NextResponse.json(
+        { error: "Username already exists" },
+        { status: 409 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
