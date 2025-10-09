1cd510f94ebf236afa44432a539bf0ed56802d51
Ofonna Nweze <nwezeofonna@gmail.com>
2025-10-08 23:10:12 -0500

    Refactor useSignUpMutation to improve error handling with AxiosError type

--- src/features/auth/use_sign_up_mutation.ts
+++ src/features/auth/use_sign_up_mutation.ts
@@ -5,19 +5,30 @@ import {
 } from "@tanstack/react-query";
 import type { SignUpFormData } from "../signup/schema";
 import { signUpApi } from "./api";
-import type { SignUpResponseData } from "../../types/api";
+import type { ApiResponseError, SignUpResponseData } from "../../types/api";
+import { AxiosError } from "axios";
 
 // Base type for sign up mutation input
 type SignUpInput = Omit<SignUpFormData, "confirmPassword">;
 
 // Hook parameter interface using object parameters
 interface UseSignUpMutationParams {
-  options?: UseMutationOptions<SignUpResponseData, Error, SignUpInput, unknown>;
+  options?: UseMutationOptions<
+    SignUpResponseData,
+    AxiosError<ApiResponseError>,
+    SignUpInput,
+    unknown
+  >;
 }
 
 export function useSignUpMutation(
   params?: UseSignUpMutationParams
-): UseMutationResult<SignUpResponseData, Error, SignUpInput, unknown> {
+): UseMutationResult<
+  SignUpResponseData,
+  AxiosError<ApiResponseError>,
+  SignUpInput,
+  unknown
+> {
   return useMutation({
     mutationFn: async (userData: SignUpInput) =>
       signUpApi({ Payload: userData }),
