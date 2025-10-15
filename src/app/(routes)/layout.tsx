31d3478279631ee4b9116f3b0b097b46f9a01aaa
Ofonna Nweze <nwezeofonna@gmail.com>
2025-10-15 10:22:01 -0500

    Remove unused SVG files and add web app manifest
    - Deleted the following SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.
    - Added new web app manifest (site.webmanifest) for the Auth Service with icons and theme colors.
    - Added new PNG files for web app manifest icons (192x192 and 512x512).
    - Updated layout.tsx to include favicon links and the web app manifest.

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -26,6 +26,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+          type="image/png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
